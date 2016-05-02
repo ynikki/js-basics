597372da46944bdb41609cbf1e85020b14603046
Nikki Y Kobayashi <nikz@MacBook-Pro.local>
2016-05-01 17:44:46 -1000

    Created a variable for myName and person

--- basics.js
+++ basics.js
@@ -1,13 +1,21 @@
 /* Create a `myName` variable and assign it a String value */
 
+var myName = 'Nikki';
+
 /* Create a `person` variable and give it 2 properties,
  * `name`, assign it the same name as before,
  * as well as an `age` (number);
  */
+var person = {
+	name: 'Nikki',
+	age: 27
+};
 
 /* Create a variable called `canDrive`,
  * if it should be true if your person object is at least 16 years old
  */
+
+var canDrive = 16;
 
 /* Create a function called `greet`,
  * it should take a 1 parameter, `name`
